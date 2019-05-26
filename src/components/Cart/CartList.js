0a4ca9249a6af1fd7ad3b1a600904f42716310f9
Laszlo Pal <pall.laszlo@web.de>
2019-05-26 17:13:42 +0200

    first phone commit

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItem from "./CartItem";
+export default function CarList({ value }) {
+  const { cart } = value;
+  return (
+    <div className="container-fluid">
+      {cart.map(item => {
+        return <CartItem key={item.id} item={item} value={value} />;
+      })}
+    </div>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link text-capitalize">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            my cart
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
+}
+const NavWrapper = styled.nav`
+  background: #2a2a72;
+  .nav-link {
+    color: #f3f3f3 !important;
+    font-size: 1.3rem;
+  }
+`;
